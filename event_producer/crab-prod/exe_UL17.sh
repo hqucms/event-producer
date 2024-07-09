0239a5c2d294bed4087428ac8cce732cf603ed69
Huilin Qu <huilin.qu@cern.ch>
2024-07-09 16:43:55 +0200

    Add ttH fragments.

---
+++ event_producer/crab-prod/exe_UL17.sh
@@ -0,0 +1,211 @@
+#!/bin/bash -x
+
+## NOTE: difference made w.r.t. common exe script
+## 3. seeds have width 100
+
+JOBINDEX=${1##*=} # hard coded by crab
+NEVENTS=${2##*=}  # ordered by crab.py script
+NTHREAD=${3##*=}  # ordered by crab.py script
+NAME=${4##*=}     # ordered by crab.py script
+BEGINSEED=${5##*=}
+
+LUMISTART=$((${BEGINSEED} + ${JOBINDEX}))
+EVENTSTART=$(((${BEGINSEED} + ${JOBINDEX}) * NEVENTS))
+SEED=$((((${BEGINSEED} + ${JOBINDEX})) * NTHREAD * 4 + 1001)) # Space out seeds; Madgraph concurrent mode adds idx(thread) to random seed
+
+WORKDIR=$(pwd)
+
+############ LHEGEN ############
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export RELEASE=CMSSW_10_6_40
+if [ -r $RELEASE/src ]; then
+  echo release $RELEASE already exists
+else
+  scram p CMSSW $RELEASE
+fi
+cd $RELEASE/src
+eval $(scram runtime -sh)
+
+# copy the fragment
+mkdir -pv $CMSSW_BASE/src/Configuration/GenProduction/python
+cp $WORKDIR/inputs/${NAME}.py $CMSSW_BASE/src/Configuration/GenProduction/python/${NAME}.py
+if [ ! -f "$CMSSW_BASE/src/Configuration/GenProduction/python/${NAME}.py" ]; then
+  echo "Fragment copy failed"
+  exit 1
+fi
+scram b -j $NTHREAD
+eval $(scram runtime -sh)
+
+cd $WORKDIR
+
+# [NOTE] need to specify seeds otherwise gridpacks will be chosen from the same routine!!
+# remember to identify process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${SEED})"!!
+cmsDriver.py Configuration/GenProduction/python/${NAME}.py \
+  --python_filename "RunIISummer20UL17wmLHE_${NAME}_cfg.py" \
+  --eventcontent RAWSIM,LHE \
+  --customise Configuration/DataProcessing/Utils.addMonitoring \
+  --datatier GEN,LHE \
+  --fileout "file:RunIISummer20UL17wmLHE_$NAME_$JOBINDEX.root" \
+  --conditions 106X_mc2017_realistic_v6 \
+  --beamspot Realistic25ns13TeVEarly2017Collision \
+  --step LHE,GEN \
+  --geometry DB:Extended \
+  --era Run2_2017 \
+  --nThreads $NTHREAD \
+  --customise_commands "process.source.numberEventsInLuminosityBlock=cms.untracked.uint32(1000)\\nprocess.source.firstLuminosityBlock=cms.untracked.uint32(${LUMISTART})\\nprocess.source.firstEvent=cms.untracked.uint64(${EVENTSTART})\\nprocess.RandomNumberGeneratorService.externalLHEProducer.initialSeed=${SEED}" \
+  --mc \
+  -n $NEVENTS || exit $?
+
+############ SIM ############
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export RELEASE=CMSSW_10_6_17_patch1
+if [ -r $RELEASE/src ]; then
+  echo release $RELEASE already exists
+else
+  scram p CMSSW $RELEASE
+fi
+cd $RELEASE/src
+eval $(scram runtime -sh)
+cd $WORKDIR
+
+cmsDriver.py \
+  --python_filename "RunIISummer20UL17SIM_${NAME}_cfg.py" \
+  --eventcontent RAWSIM \
+  --customise Configuration/DataProcessing/Utils.addMonitoring \
+  --datatier GEN-SIM \
+  --fileout "file:RunIISummer20UL17SIM_$NAME_$JOBINDEX.root" \
+  --conditions 106X_mc2017_realistic_v6 \
+  --beamspot Realistic25ns13TeVEarly2017Collision \
+  --step SIM \
+  --geometry DB:Extended \
+  --filein "file:RunIISummer20UL17wmLHE_$NAME_$JOBINDEX.root" \
+  --era Run2_2017 \
+  --runUnscheduled \
+  --mc \
+  --nThreads $NTHREAD \
+  -n $NEVENTS || exit $?
+
+############ DIGIPremix ############
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export RELEASE=CMSSW_10_6_17_patch1
+if [ -r $RELEASE/src ]; then
+  echo release $RELEASE already exists
+else
+  scram p CMSSW $RELEASE
+fi
+cd $RELEASE/src
+eval $(scram runtime -sh)
+cd $WORKDIR
+
+cmsDriver.py \
+  --python_filename "RunIISummer20UL17DIGIPremix_${NAME}_cfg.py" \
+  --eventcontent PREMIXRAW \
+  --customise Configuration/DataProcessing/Utils.addMonitoring \
+  --datatier GEN-SIM-DIGI \
+  --filein "file:RunIISummer20UL17SIM_$NAME_$JOBINDEX.root" \
+  --fileout "file:RunIISummer20UL17DIGIPremix_$NAME_$JOBINDEX.root" \
+  --pileup_input dbs:/Neutrino_E-10_gun/RunIISummer20ULPrePremix-UL17_106X_mc2017_realistic_v6-v3/PREMIX \
+  --conditions 106X_mc2017_realistic_v6 \
+  --step DIGI,DATAMIX,L1,DIGI2RAW \
+  --procModifiers premix_stage2 \
+  --geometry DB:Extended \
+  --datamix PreMix \
+  --era Run2_2017 \
+  --runUnscheduled \
+  --mc \
+  --nThreads $NTHREAD \
+  -n $NEVENTS || exit $?
+
+############ HLT ############
+export SCRAM_ARCH=slc7_amd64_gcc630
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export RELEASE=CMSSW_9_4_14_UL_patch1
+if [ -r $RELEASE/src ]; then
+  echo release $RELEASE already exists
+else
+  scram p CMSSW $RELEASE
+fi
+cd $RELEASE/src
+eval $(scram runtime -sh)
+cd $WORKDIR
+
+cmsDriver.py \
+  --python_filename "RunIISummer20UL17HLT_${NAME}_cfg.py" \
+  --eventcontent RAWSIM \
+  --customise Configuration/DataProcessing/Utils.addMonitoring \
+  --datatier GEN-SIM-RAW \
+  --filein "file:RunIISummer20UL17DIGIPremix_$NAME_$JOBINDEX.root" \
+  --fileout "file:RunIISummer20UL17HLT_$NAME_$JOBINDEX.root" \
+  --conditions 94X_mc2017_realistic_v15 \
+  --customise_commands 'process.source.bypassVersionCheck = cms.untracked.bool(True)' \
+  --step HLT:2e34v40 \
+  --geometry DB:Extended \
+  --era Run2_2017 \
+  --mc \
+  -n $NEVENTS || exit $?
+
+############ RECO ############
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export RELEASE=CMSSW_10_6_17_patch1
+if [ -r $RELEASE/src ]; then
+  echo release $RELEASE already exists
+else
+  scram p CMSSW $RELEASE
+fi
+cd $RELEASE/src
+eval $(scram runtime -sh)
+cd $WORKDIR
+
+cmsDriver.py \
+  --python_filename "RunIISummer20UL17RECO_${NAME}_cfg.py" \
+  --eventcontent AODSIM \
+  --customise Configuration/DataProcessing/Utils.addMonitoring \
+  --datatier AODSIM \
+  --filein "file:RunIISummer20UL17HLT_$NAME_$JOBINDEX.root" \
+  --fileout "file:RunIISummer20UL17RECO_$NAME_$JOBINDEX.root" \
+  --conditions 106X_mc2017_realistic_v6 \
+  --step RAW2DIGI,L1Reco,RECO,RECOSIM \
+  --geometry DB:Extended \
+  --era Run2_2017 \
+  --runUnscheduled \
+  --mc \
+  --nThreads $NTHREAD \
+  -n $NEVENTS || exit $?
+
+############ MiniAODv2 ############
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export RELEASE=CMSSW_10_6_20
+if [ -r $RELEASE/src ]; then
+  echo release $RELEASE already exists
+else
+  scram p CMSSW $RELEASE
+fi
+cd $RELEASE/src
+eval $(scram runtime -sh)
+cd $WORKDIR
+
+## for the last step, needs to run with -j FrameworkJobReport.xml
+cmsDriver.py \
+  --python_filename "RunIISummer20UL17MINIAODSIM_${NAME}_cfg.py" \
+  --eventcontent MINIAODSIM \
+  --customise Configuration/DataProcessing/Utils.addMonitoring \
+  --datatier MINIAODSIM \
+  --filein "file:RunIISummer20UL17RECO_$NAME_$JOBINDEX.root" \
+  --fileout file:miniv2.root \
+  --conditions 106X_mc2017_realistic_v9 \
+  --step PAT \
+  --procModifiers run2_miniAOD_UL \
+  --geometry DB:Extended \
+  --era Run2_2017 \
+  --runUnscheduled \
+  --no_exec \
+  --nThreads $NTHREAD \
+  --mc \
+  -n $NEVENTS
+
+cmsRun -j FrameworkJobReport.xml "RunIISummer20UL17MINIAODSIM_${NAME}_cfg.py"
---
+++ event_producer/crab-prod/inputs/TTHTo2Q_TTTo2L2Nu_M-160_TuneCP5_13TeV-powheg-pythia8.py
@@ -0,0 +1,53 @@
+import FWCore.ParameterSet.Config as cms
+
+externalLHEProducer = cms.EDProducer(
+    "ExternalLHEProducer", args=cms.vstring(
+        '/eos/cms/store/cmst3/user/hqu/gridpacks/ttH/ttH_slc7_amd64_gcc700_CMSSW_10_6_30_patch1_my_ttH_ttTo2L2Nu_hdamp_NNPDF31_13TeV_M160.tgz'),
+    nEvents=cms.untracked.uint32(5000),
+    generateConcurrently=cms.untracked.bool(True),
+    numberOfParameters=cms.uint32(1),
+    outputFile=cms.string('cmsgrid_final.lhe'),
+    scriptName=cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh'))
+
+# Link to datacards:
+# https://github.com/cms-sw/genproductions/blob/master/bin/Powheg/production/2017/13TeV/Higgs/ttH_ttTo2L2Nu_hdamp_NNPDF31_13TeV_M125/ttH_ttTo2L2Nu_hdamp_NNPDF31_13TeV_M125.input
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
+                         maxEventsToPrint=cms.untracked.int32(1),
+                         pythiaPylistVerbosity=cms.untracked.int32(1),
+                         filterEfficiency=cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity=cms.untracked.bool(False),
+                         comEnergy=cms.double(13000.),
+                         PythiaParameters=cms.PSet(
+                             pythia8CommonSettingsBlock,
+                             pythia8CP5SettingsBlock,
+                             pythia8PowhegEmissionVetoSettingsBlock,
+                             pythia8PSweightsSettingsBlock,
+                             processParameters=cms.vstring(
+                                 'POWHEG:nFinal = 3',  # Number of final state particles
+                                 # (BEFORE THE DECAYS) in the LHE
+                                 # other than emitted extra parton
+                                 '23:mMin = 0.05',
+                                 '24:mMin = 0.05',
+                                 '25:m0 = 160.0',
+                                 # H->bb or H->cc decays
+                                 '25:onMode = off',
+                                 '25:oneChannel = 1 0.5 100 5 -5',
+                                 '25:addChannel = 1 0.5 100 4 -4',
+                                 'ResonanceDecayFilter:filter = on'
+                             ),
+                             parameterSets=cms.vstring('pythia8CommonSettings',
+                                                       'pythia8CP5Settings',
+                                                       'pythia8PSweightsSettings',
+                                                       'pythia8PowhegEmissionVetoSettings',
+                                                       'processParameters'
+                                                       )
+                         )
+                         )
+
+ProductionFilterSequence = cms.Sequence(generator)
